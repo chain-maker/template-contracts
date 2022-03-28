0c21963d3e663392d33999b113b3a84cfd7e2238
ray <ray@example.com>
2022-03-28 14:49:47 +0800

    remov deploy initSupply

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ task("check-balance", "Prints out the balance of your account").setAction(async 
 
 task("deployERC20", "Deploys the ERC20Template.sol contract").setAction(async function (taskArguments, hre) {
     const contractFactory = await hre.ethers.getContractFactory("ERC20Template", getAccount());
-    const erc20 = await contractFactory.deploy("ERC20 Token", "TT", 6, 10000000000, "0x2a17E171D110E8C62562F84837B1E3b55159b05B");
+    const erc20 = await contractFactory.deploy("ERC20 Token", "TT", 6, "0x2a17E171D110E8C62562F84837B1E3b55159b05B");
     console.log(`Contract deployed to address: ${erc20.address}`);
 })
 
